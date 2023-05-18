e4b5f3c08798aeeb3c309fa59166d96d3d97b575
Orcun Erdil <orcun_erdil@hotmail.com>
2023-05-18 18:01:47 +0300

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Orçun Erdil on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
